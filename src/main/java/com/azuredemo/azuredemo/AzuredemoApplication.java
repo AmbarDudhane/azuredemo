15e7218e6f42b28c1b555d41eaa5c65387ebc878
AmbarDudhane <ambarsd12345@hotmail.com>
2020-05-24 20:50:08 -0500

    Update AzuredemoApplication.java

--- src/main/java/com/azuredemo/azuredemo/AzuredemoApplication.java
+++ src/main/java/com/azuredemo/azuredemo/AzuredemoApplication.java
@@ -1,8 +1,9 @@
 package com.azuredemo.azuredemo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 
@@ -10,6 +11,7 @@ import democontroller.DemoController;
 
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @ComponentScan(basePackageClasses = DemoController.class)
 //@EnableJpaRepositories("demoncontroller")
 //@EntityScan("democontroller")
